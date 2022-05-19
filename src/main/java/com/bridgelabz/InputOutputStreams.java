2f57fa385e866d6e30175080861071bb5825a9ee
k12e <kalyanirathod2095@gmail.com>
2022-05-19 14:56:44 +0530

    Added: write to file

--- src/main/java/com/bridgelabz/InputOutputStreams.java
+++ src/main/java/com/bridgelabz/InputOutputStreams.java
@@ -1,14 +1,31 @@
 package com.bridgelabz;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class InputOutputStreams {
     public static void main(String[] args) {
         createFile();
         getFileInformation();
-       // writingToFile();
-        //readFile();
+        writingToFile();
+      //  readFile();
+    }
+
+
+    private static void writingToFile() {
+    File file=new File("C:\\Users\\JJadhao\\IdeaProjects\\untitled\\EmployeePayRolll\\src\\test.txt");
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            String contentOfFile="In Java, a File is an abstract data type." +
+                    " A named location used to store related information is known as a File. " +
+                    "There are several File Operations like creating a new File, getting information about File, " +
+                    "writing into a File, reading from a File and deleting a File.";
+            fileWriter.write(contentOfFile);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void createFile() {
