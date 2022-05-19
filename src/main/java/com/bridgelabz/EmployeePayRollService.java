16108606728925cd3d18fa431581aee18f8c1719
k12e <kalyanirathod2095@gmail.com>
2022-05-19 13:36:45 +0530

    Added: create new file

---
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayRollService {
+    public enum IOService{console_IO,File_IO}
+    private List<EmployeePayRollData> employeePayRollList;
+   public EmployeePayRollService(){}
+
+    public EmployeePayRollService(List<EmployeePayRollData>employeePayRollList){}
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayRollData> employeePayRollList=new ArrayList<>();
+        EmployeePayRollService employeePayRollService=new EmployeePayRollService(employeePayRollList);
+        Scanner condoleInputReader
+    }
+}
---
+++ src/main/java/com/bridgelabz/InputOutputStreams.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.io.File;
+import java.io.IOException;
+
+public class InputOutputStreams {
+    public static void main(String[] args) {
+        createFile();
+
+    }
+    private static void createFile() {
+        File file=new File("C:\\Users\\JJadhao\\IdeaProjects\\untitled\\EmployeePayRolll\\src\\test.txt");
+        try{
+            if(file.createNewFile()){
+                System.out.println("file created "+file.getName());
+            }
+            else {
+                System.out.println("file exist"+file.getName());
+            }
+        }catch (IOException e){
+            System.out.println("error is "+e);
+
+        }
+    }
+
+}
