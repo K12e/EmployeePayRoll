00e3243c590b1953222e77ca9fa87a401e5942b8
k12e <kalyanirathod2095@gmail.com>
2022-05-20 13:57:24 +0530

    Added: Read and Write EmployeePayRoll To a Console

--- src/main/java/com/bridgelabz/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -10,10 +10,24 @@ public class EmployeePayRollService {
    public EmployeePayRollService(){}
 
     public EmployeePayRollService(List<EmployeePayRollData>employeePayRollList){}
-
     public static void main(String[] args) {
         ArrayList<EmployeePayRollData> employeePayRollList=new ArrayList<>();
         EmployeePayRollService employeePayRollService=new EmployeePayRollService(employeePayRollList);
-        Scanner condoleInputReader;
+        Scanner consoleInputReader=new Scanner(System.in);
+        employeePayRollService.readEmployeePayRollData(consoleInputReader);
+        employeePayRollService.writeEmployeePayRollData();
     }
+
+    private void readEmployeePayRollData(Scanner consoleInputReader) {
+        System.out.println("Enter employee ID");
+        int id=consoleInputReader.nextInt();
+        System.out.println("Enter employee Name");
+        String name=consoleInputReader.next();
+        System.out.println("Enter employee Salary");
+        double salary=consoleInputReader.nextDouble();
+        employeePayRollList.add(new EmployeePayRollData(id,name,salary));
+    }
+    private void writeEmployeePayRollData() {
+        System.out.println("\nWriting Employee Pay Roll Roster to Console\n"+employeePayRollList);
+   }
 }
---
+++ src/test/java/com/bridgelabz/EmployeePayRollServiceTest.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeePayRollServiceTest {
+@Test
+    public void givenFileOnReading
+}
