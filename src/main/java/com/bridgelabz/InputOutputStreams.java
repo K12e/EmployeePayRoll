29a3b03e94c8c932ca46112adabd47030c355942
k12e <kalyanirathod2095@gmail.com>
2022-05-20 11:59:15 +0530

    Added: write file opration

--- src/main/java/com/bridgelabz/InputOutputStreams.java
+++ src/main/java/com/bridgelabz/InputOutputStreams.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
 public class InputOutputStreams {
     public static void main(String[] args) {
         createFile();
@@ -12,7 +11,33 @@ public class InputOutputStreams {
       //  readFile();
     }
 
+    private static void createFile() {
+        File file=new File("C:\\Users\\JJadhao\\IdeaProjects\\untitled\\EmployeePayRolll\\src\\test.txt");
+            try{
+                if(file.createNewFile()){
+                    System.out.println("file created "+file.getName());
+                }
+                else {
+                    System.out.println("file exist"+file.getName());
+                }
+            }catch (IOException e){
+                System.out.println("error is "+e);
 
+            }
+        }
+        private static void getFileInformation() {
+        File file=new File("C:\\Users\\JJadhao\\IdeaProjects\\untitled\\EmployeePayRolll\\src\\test.txt");
+            if(file.exists()) {
+                System.out.println("File name " + file.getName());
+                System.out.println("Readable " + file.canRead());
+                System.out.println("Writable " + file.canWrite());
+                System.out.println("Excecutable " + file.canExecute());
+                System.out.println("Absolute path " + file.getAbsolutePath());
+                System.out.println("path " + file.getPath());
+                System.out.println("path " + file.length());
+            }
+
+     }
     private static void writingToFile() {
     File file=new File("C:\\Users\\JJadhao\\IdeaProjects\\untitled\\EmployeePayRolll\\src\\test.txt");
         try {
@@ -28,33 +53,7 @@ public class InputOutputStreams {
         }
     }
 
-    private static void createFile() {
-        File file=new File("C:\\Users\\JJadhao\\IdeaProjects\\untitled\\EmployeePayRolll\\src\\test.txt");
-        try{
-            if(file.createNewFile()){
-                System.out.println("file created "+file.getName());
-            }
-            else {
-                System.out.println("file exist"+file.getName());
-            }
-        }catch (IOException e){
-            System.out.println("error is "+e);
 
-        }
-    }
-
-    private static void getFileInformation() {
-        File file1 = new File("C:\\Users\\JJadhao\\IdeaProjects\\untitled\\EmployeePayRolll\\src\\test.txt");
-        if (file1.exists()) {
-            System.out.println("File name " + file1.getName());
-            System.out.println("Readable " + file1.canRead());
-            System.out.println("Writable " + file1.canWrite());
-            System.out.println("Excecutable " + file1.canExecute());
-            System.out.println("Absolute path " + file1.getAbsolutePath());
-            System.out.println("path " + file1.getPath());
-            System.out.println("path " + file1.length());
-        }
-    }
 }
 
 
